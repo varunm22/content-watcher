13489c1fc6ee791ddbe90c85daf3151128d37909
Anjelica Solomon <67875952+anjelicas@users.noreply.github.com>
2020-07-20 15:13:46 -0400

    Update online_scraper.py

--- online_scraper.py
+++ online_scraper.py
@@ -1,16 +1,21 @@
-#this runs on your local webserver
-from flask import Flask, request #import main Flask class and request object
+from flask import Flask, redirect, url_for, render_template, request, session
 from urllib.request import urlopen # lets us request a url
 from urllib.error import HTTPError
 import urllib.request
 from bs4 import BeautifulSoup
+
 app = Flask(__name__) #create the Flask app
+app.secret_key = "webscraper"
+
+@app.route("/")
+def scraper():
+    return render_template("index.html")
 
 @app.route('/', methods=['GET', 'POST']) #allow both GET and POST requests
 def form_example():
     if request.method == 'POST': #this block is only entered when the form is submitted
         website = request.form.get('website')
-        topic = request.form['topic']
+        topic = request.form.get('topic')
         
         #code for webscraper, takes the information that user enters through the form
         html = urllib.request.urlopen(website).read()
@@ -23,20 +28,18 @@ def form_example():
         text = soup.get_text()
 
         #counts number of results of topic/item on page & ignores case sensitivty
-        results = text.lower().count(topic)
+        results = str(text.lower().count(topic))
         
         #prints results
-        answer = 'Website: ' + website + '.\n' + 'Number of results for ' + topic + ': ' + str(results) + '.'
-        print(answer)
+        #answer = 'Website: ' + website + '.\n' + 'Number of results for ' + topic + ': ' + results + '.'
 
-        return answer
+        return '''<p> Website: {}</p>
+                <p> Search Term: {}</p>
+                <p> Number of Results: {}</p>'''.format(website, topic, results)
         
     #form shown on webserver/site that prompts user to enter information
-    return '''<form method="POST">
-                  Website: <input type="text" name="website"><br>
-                  Topic: <input type="text" name="topic"><br>
-                  <input type="submit" value="Submit"><br>
-              </form>'''
+    return render_template("index.html")
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
+if __name__ == "__main__":
+    app.run(debug=True) 
