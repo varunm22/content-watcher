88f5933e0c49a7b024d40039b2e9cfbaf47f95b0
Anjelica Solomon <67875952+anjelicas@users.noreply.github.com>
2020-07-23 14:52:56 -0400

    Add files via upload

---
+++ queryentry_server.py
@@ -0,0 +1,17 @@
+#this is a separate webserver for only query entries that works independently of itself, 
+#a success message will appear when a post request is successfully made
+
+from flask import Flask, redirect, url_for, render_template, request, session
+
+app = Flask(__name__) #create the Flask app
+app.secret_key = "queryentry"
+
+@app.route('/', methods=['GET', 'POST']) #allow both GET and POST requests
+def form_example():
+    if request.method == 'POST': #this block is only entered when the form is submitted
+        return "Success! Information entered!"  
+    #form shown on webserver/site that prompts user to enter information
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5001) 
