af78ef815158a15e6feaef0d523e756fa590826f
YEEHAW1991 <67877705+YEEHAW1991@users.noreply.github.com>
2020-07-25 21:33:32 -0400

    Update scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -2,67 +2,36 @@ from flask import Flask
 import schedule
 import time
 
-
-
 app = Flask(__name__)
 
-@app.route('/') #end of basic Flask stuff
+from app import routes
 
-counter = 1
+#create query
 
-seconds = 0
+#basically, depending on the type of frequency the user chooses, it will go into a certain type of route
 
-minutes = 0
+@app.route('/second/')
+def second():
+       minutes = #sortby minutes; not sure how to write it so minutes is equal to the number the user inputs; similar thing to stuff below
+       seconds = minutes * 60 #converts to seconds
 
-hours = 0
-
-def reload():
-    counter += 1
-    
+@app.route('/minute/')
 def minute():
-    if #user puts minutes:
-        seconds = minutes * 60
+       minutes = 
+       seconds = minutes * 60 #converts to seconds
 
+
+@app.route('/hour/')
 def hour():
-    if #user puts hour:
-        seconds = hours * 3600
+       hours = 
+       seconds = hours * 3600 #converts to seconds
+
+@app.route('/week/')
+def week():
+       weeks =
+       seconds = weeks * 604800 #converts to seconds
 
 schedule.every(seconds)seconds.do(reload)
 
-if #go to website:
-    return str(counter)
-
-if #refresh:
-    counter = counter * -1
-
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-#old stuff basically loops through everything from 0 to states seconds
-#def scraper():
- #   return render_Backend('scraper.py') #calls scraper
-
-#def index():
- #   return render_template('index.html') #setiing up the html
-
-#def reload():
- #   webbrowser.open_new_tab(url)
-
-#schedule.every(a)seconds.do(reload)#function
-
-
-
-#n = [] # empty list to hold numbers
-
-#m = 0
-#while m >= 0:
-#    n = n.append(str(m))
-#    m += 1 #creates an array with all positive integers
-
-#should keep in mind that frequency will be kept in seconds
-#def frequency(a):
-#    for x in n:
-        #if keyword is in within this frequency, print 'Yes'
-        #if not, print 'No'
-        #this loop will loop through all seconds from 0 to the user's stated times
