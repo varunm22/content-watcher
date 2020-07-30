e0f8184ce032ce38c8f1cba022fd2a1739dff3a9
Anjelica Solomon <67875952+anjelicas@users.noreply.github.com>
2020-07-29 21:19:44 -0400

    forgot to include frequency

--- queryentry_server.py
+++ queryentry_server.py
@@ -12,8 +12,9 @@ def form_example():
     if request.method == 'POST': #this block is only entered when the form is submitted
         website = request.form.get('website')
         topic = request.form.get('topic')
+        frequency = request.form.get('frequency')
         url = 'http://127.0.0.1:5000/' #sends information to scheduler server
-        myobj = {'website': website, 'topic': topic}
+        myobj = {'website': website, 'topic': topic, 'frequency': frequency}
         x = requests.post(url, json = myobj)
         return "Success! Information entered!" 
     #form shown on webserver/site that prompts user to enter information
