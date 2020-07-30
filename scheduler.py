252eef72b1e8a4ae6a99ec4d74a278feb8f95018
YEEHAW1991 <67877705+YEEHAW1991@users.noreply.github.com>
2020-07-29 21:14:23 -0400

    Update scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -29,33 +29,24 @@ def add_query():
     frequency = json['frequency']
     topic = json['topic']
 
-    # VARUN this is just sample code i wrote for you using the read_data
-    # and write_data functions I wrote above to store data. You'll need
-    # to use these to create your query dict and access/modify it
-    query_id = read_data(1, "counter")
+    query_id = read_data(1, "query_id")
     query[query_id] = {'topic' : topic, 'website' : website, 'frequency' : frequency}
     query_id += 1
-    write_data(counter, "counter")
+    write_data(query_id, "query_id")
 
     schedule.every(frequency).seconds.do(add_query).tag('create')
     return "Query created!"
 
-# VARUN: remember, all you want as an input for delete is a query id! this
-# when you're taking things out of json, all you should be taking out is
-# query id. If you don't understand what i mean, ask me!
 @app.route('/delete_query', methods=['GET', 'POST'])
 def delete_query():
-    
+
     del query['query_id']
     schedule.clear('create')
-    schedule.every(frequency).seconds.do(delete_query).tag('delete')
     return "Query deleted!"
-
 
 @app.route('/show_all', methods=['GET', 'POST'])
 def show_all():
     return query
-    schedule.every(frequency).seconds.do(show_all).tag('show')
 
 if __name__ == "__main__":
     app.run(debug=True)
